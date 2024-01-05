5564410971d8591386693c4c975ca268d9d524a7
Mukilan1win23 <mukilan.2.2014@gmail.com>
2024-01-05 07:50:18 -0600

    Updated Web

---
+++ js/jquery-ui.js
@@ -0,0 +1,5 @@
+$(document).ready(function() { 
+	$("#my-tabs").tabs({ 
+			active: 0 
+		}) 
+}) 
--- js/script.js
+++
@@ -1,7 +0,0 @@
-console.log('Website is working')
-
-
-repeat (
-    rect(123, 123, 123, 123)
-
-)
