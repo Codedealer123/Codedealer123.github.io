817f3caec904ec7e6e6ad6d00ab153b23dc66783
DustySoul1 <mukilan.2.2014@gmail.com>
2025-02-20 17:01:59 -0600

    AdminPageError

--- assets/JS/AdminCode.js
+++ assets/JS/AdminCode.js
@@ -1,6 +1,6 @@
 document.addEventListener("keydown", function(event) {
     if (event.key === "a" || event.key === "A") { 
-        document.getElementById("myLink").href = "/home/Admin";
+        document.getElementById("News").href = "/home/Admin";
         console.log("Admin Access Granted!"); // Debugging log
     }
 });
