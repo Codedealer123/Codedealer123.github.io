299d0a803870f69a265864617fa1fb991c5c99b5
DustySoul1 <mukilan.2.2014@gmail.com>
2025-03-14 12:20:25 -0500

    GamesAndZeldaUpdate

--- assets/JS/AdminCode.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("keydown", function(event) {
-    if (event.key === "a" || event.key === "a") { 
-        document.getElementById("News").href = "/home/Admin";
-        console.log("Admin Access Granted!"); // Debugging log
-    }
-});
