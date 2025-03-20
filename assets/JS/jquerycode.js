24221c440af2225171422df3135e9a7239755966
DustySoul1 <mukilan.2.2014@gmail.com>
2025-03-20 14:26:10 -0500

    AddtionalItems

--- assets/JS/jquerycode.js
+++ assets/JS/jquerycode.js
@@ -1,3 +1,8 @@
 $( function() {
     $(document).tooltip();
 });
+// Query the user for permission.
+const periodicSyncPermission = await navigator.permissions.query({
+    name: 'periodic-background-sync',
+  });
+  
